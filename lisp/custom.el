014ea53144cf89984d0caa1016f60e77e9ceb558
ButterBlock233 <Voltage15312@outlook.com>
2023-09-14 18:10:24 +0800

    更新字体

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,13 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(custom-safe-themes
    '("0c860c4fe9df8cff6484c54d2ae263f19d935e4ff57019999edbda9c7eda50b8" default))
+ '(global-display-line-numbers-mode t)
  '(package-selected-packages
-   '(doom-themes which-key treemacs-tab-bar treemacs-projectile treemacs-evil smart-mode-line-atom-one-dark-theme rust-mode rainbow-delimiters mwim evil-tutor-sc dashboard counsel company atom-one-dark-theme amx)))
+   '(doom-themes which-key treemacs-tab-bar treemacs-projectile treemacs-evil smart-mode-line-atom-one-dark-theme rust-mode rainbow-delimiters mwim evil-tutor-sc dashboard counsel company atom-one-dark-theme amx))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "JetBrains Mono" :foundry "outline" :slant normal :weight regular :height 110 :width normal)))))
