11ecc4ffe858f30e1a1db3614a3af23db3dba506
ButterBlock233 <Voltage15312@outlook.com>
2023-10-08 12:21:33 +0800

    用于保存加载路径的load-path.el

--- lisp/load-path.el
+++ lisp/load-path.el
@@ -1,4 +1,19 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/lsp-bridge")
 (setq custom-file "~/.emacs.d/lisp/custom.el")
 (load custom-file)
+
+;====================Straight.el====================
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;===================================================
 (provide 'load-path)
